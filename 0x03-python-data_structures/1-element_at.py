1113fbe3c3b8b44214f28676cd53c80916e24384
Ali-TBB <Ali-Tbb@git.com>
2024-01-08 13:14:26 +0100

    task 1

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def element_at(my_list, idx):
     i = len(my_list)
-    if (idx < 0) or (i < idx):
-        return None
+    if (idx < 0) or (i <= idx):
+        return
     else:
         return my_list[idx]
