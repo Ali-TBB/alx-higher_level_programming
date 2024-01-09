67031ae307de1e32db71dd43b0b2a47b07ebb76c
Ali-TBB <Ali-Tbb@git.com>
2024-01-09 20:19:02 +0100

    task 10

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -6,7 +6,7 @@ def divisible_by_2(my_list=[]):
 
 	for i in range(0, len(my_list)):
 		if ((my_list[i] % 2 ) == 0):
-			new_list[i] = True
+			new_list[i] = 1
 		else:
-			new_list[i] = False
+			new_list[i] = 0
 	return new_list
