0410f3761ec4dea29ebada99dc2bcc8c853e48c5
Ali-TBB <Ali-Tbb@git.com>
2024-01-10 11:29:08 +0100

    task 1

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,4 +1,10 @@
 #!/usr/bin/python3
 
-
 def search_replace(my_list, search, replace):
+    new_list = my_list.copy()
+    for i in range(0, len(my_list)):
+        if new_list[i] == search:
+            new_list[i] = replace
+        else:
+            continue
+    return new_list
