7160c117efd0ac59150dd1c074992364cb37eb42
Ali-TBB <Ali-Tbb@git.com>
2024-01-10 12:32:48 +0100

    task 10

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,10 +1,8 @@
 #!/usr/bin/python3
 
 def best_score(a_dictionary):
-    if a_dictionary and len(a_dictionary):
-        max = list(a_dictionary.key())[0]
-        for key in a_dictionary:
-            if a_dictionary[key] > a_dictionary[max]:
-                max = key
-        return max
-    return None
+    if not a_dictionary:
+        return None
+
+    max_key = max(a_dictionary, key=a_dictionary.get)
+    return max_key
