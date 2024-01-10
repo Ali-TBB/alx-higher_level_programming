a05ef130391bfd45de55853cbd78a2b8615838c9
Ali-TBB <Ali-Tbb@git.com>
2024-01-10 13:38:45 +0100

    task 13

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -1,1 +1,12 @@
 #!/usr/bin/python3
+
+def weight_average(my_list=[]):
+    if my_list and len(my_list):
+        result = 0
+        for i in my_list:
+            result += i[0] * i[1]
+        dev = 0
+        for j in my_list:
+            dev += j[1]
+        return (result / dev)
+    return 0
