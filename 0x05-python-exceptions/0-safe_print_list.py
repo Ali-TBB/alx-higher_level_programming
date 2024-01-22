1250b391f44e939c56172a34d01c708aa2c38d55
Ali-TBB <Ali-Tbb@git.com>
2024-01-22 20:41:00 +0100

    task 0

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 
 def safe_print_list(my_list=[], x=0):
-    len = 0
-    while(x >= len):
-        try:
-            print(my_list[x], end='')
-            len += 1
-        except:
-            break
-    return len
+	len = 0
+	try:
+		for i in range(x):
+			print(my_list[x], end='')
+			len += 1
+	except IndexError:
+		print()
+	return len
