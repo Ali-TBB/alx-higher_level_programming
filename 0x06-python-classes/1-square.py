21189108b510fbbb4531517d21711d7e9b3ddeb3
Ali-TBB <Ali-Tbb@git.com>
2024-01-24 10:19:30 +0100

    task 1

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """Square class take size as private attribute"""
 class Square:
+    """this is private attribute"""
     def __init__(self, arg):
-        """this is private attribute"""
         self.__size = arg
