23e4c66555b88dceba363a8867ff4739f52143ff
Ali-TBB <Ali-Tbb@git.com>
2024-01-24 11:15:08 +0100

    task 4

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -3,17 +3,18 @@
 class Square:
     """This class represents a square with a private attribute 'size'."""
     def __init__(self, size=0):
+        # Private instance attribute
         self.__size = None
         self.set_size(size)
-
-    def set_size(self, new_size):
+    @size.setter
+    def size(self, value):
         """Setter method for the 'size' attribute."""
-        if not isinstance(new_size, int):
+        if not isinstance(value, int):
             raise TypeError("size must be an integer")
-        if new_size < 0:
+        if value < 0:
             raise ValueError("size must be >= 0")
 
-        self.__size = new_size
+        self.__size = value
 
     def area(self):
         """Calculate and return the area of the square."""
---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""Square class take size as private attribute"""
+class Square:
+    """This class represents a square with a private attribute 'size'."""
+    def __init__(self, size=0):
+        # Private instance attribute
+        self.__size = None
+        self.set_size(size)
+    def size(self):
+        """Getter method for the 'size' attribute."""
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """Setter method for the 'size' attribute."""
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+
+        self.__size = value
+
+    def area(self):
+        """Calculate and return the area of the square."""
+        return self.__size ** 2
