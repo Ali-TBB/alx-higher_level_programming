cf98712f57d8d81e5e62080bb514b6927658e428
Ali-TBB <Ali-Tbb@git.com>
2024-01-24 10:29:40 +0100

    task 2

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""Square class take size as private attribute"""
+class Square:
+    """this is private attribute"""
+    def __init__(self, size=0):
+        self.__size = None
+        self.set_size(size)
+
+    def set_size(self, new_size):
+        if not isinstance(new_size, int):
+            raise TypeError("size must be an integer")
+        if new_size < 0:
+            raise ValueError("size must be >= 0")
+
+        self.__size = new_size
