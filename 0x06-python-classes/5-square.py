b25e113e73c4e38a7b65091a7a74d2fd6c923f40
Ali-TBB <Ali-Tbb@git.com>
2024-01-24 11:33:56 +0100

    task 5

---
+++ 0x06-python-classes/5-square.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+"""Square class take size as private attribute"""
+class Square:
+    """This class represents a square with a private attribute 'size'."""
+    def __init__(self, size=0):
+        # Private instance attribute
+        self.__size = None
+        self.size = size
+    @property
+    def size(self):
+        """Getter method for the 'size' attribute."""
+        return self.__size
+    @size.setter
+    def size(self, value):
+        """Setter method for the 'size' attribute."""
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+
+        self.__size = value
+
+    def area(self):
+        """Calculate and return the area of the square."""
+        return self.__size ** 2
+    def my_print(self):
+        if self.size == 0:
+            print()
+        else:
+            for i in range(self.size):
+                for j in range(self.size):
+                    print("#", end="")
+                print()
