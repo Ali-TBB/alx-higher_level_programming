1392bf185ea79be2e24ecb1749a2a9fa40af432f
Ali-TBB <Ali-Tbb@git.com>
2024-01-30 09:30:49 +0100

    task 1

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -10,6 +10,8 @@ Author:
 Date:
 [2024/01/30]
 """
+
+
 class Rectangle:
     """the Rectangle class."""
     def __init__(self, width=0, height=0):
@@ -24,19 +26,29 @@ class Rectangle:
         self.__height = height
     @property
     def width(self):
+        """getter for the private instance attribute width"""
         return self.__width
+
+
     @width.setter
     def width(self, value):
+        """setter for the private instance attribute width"""
         if not isinstant(value, int):
             raise TypeError("width must be an integer")
         if value < 0:
             raise ValueError("width must be >= 0")
         self.__width = value
+
+
     @property
     def height(self):
+        """getter for the private instance attribute height"""
         return self.__height
+
+
     @height.setter
     def height(self, value):
+        """setter for the private instance attribute height"""
         if not isinstant(value, int):
             raise TypeError("height must be an integer")
         if value < 0:
