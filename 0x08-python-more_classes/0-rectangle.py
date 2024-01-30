0595dec35965494157d9995d7b3023a9e20ac928
Ali-TBB <Ali-Tbb@git.com>
2024-01-30 09:06:27 +0100

    task 0

---
+++ 0x08-python-more_classes/0-rectangle.py
@@ -0,0 +1,17 @@
+#!/bin/python3
+"""
+Rectangle Class
+
+This is empty class.
+
+Author:
+[ali debbache]
+
+Date:
+[2024/01/29]
+"""
+class Rectangle:
+    """
+    the Rectangle class.
+    """
+    pass
