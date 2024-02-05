543129316a58ba6d2bef17096aa6ea2927cdcf5e
Ali-TBB <Ali-Tbb@git.com>
2024-02-05 20:26:07 +0100

    task 8

---
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+Rectangle class.
+class Rectangle that inherits from BaseGeometry.
+Author:
+[ali debbache]
+Date:
+[2024/02/05]
+"""
+
+class Rectangle(BaseGeometry):
+    """ Rectangle class"""
+    def __init__(self, width, height):
+        """ instantiation  function"""
+        self.__width = width
+        self.integer_validator(width, width)
+        self.__height = height
+        self.integer_validator(height, height)
