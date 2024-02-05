ee0aa05315b0ea18c55d760a8ece481d23f26577
Ali-TBB <Ali-Tbb@git.com>
2024-02-05 19:19:33 +0100

    task 6

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+BaseGeometry class.
+class that Raises an Exception with the message
+'area() is not implemented'.
+Author:
+[ali debbache]
+Date:
+[2024/02/05]
+"""
+
+
+class BaseGeometry:
+
+    def area(self):
+        """
+        Raises an Exception with the message 'area() is not implemented'.
+        """
+        raise Exception("area() is not implemented")
