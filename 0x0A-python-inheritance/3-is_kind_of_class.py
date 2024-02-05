78169478b5e7247046d92778841fbba82bdab03d
Ali-TBB <Ali-Tbb@git.com>
2024-02-05 19:11:08 +0100

    task 4

--- 0x0A-python-inheritance/3-is_kind_of_class.py
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """
-is_same_class function.
+is_kind_of_class function.
 function that returns True if the object is exactly an instance
 of the specified class ; otherwise False.
 Author:
---
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+inherits_from function.
+function that returns True if the object is exactly an instance
+of the specified class ; otherwise False.
+Author:
+[ali debbache]
+Date:
+[2024/02/05]
+"""
+
+
+def inherits_from(obj, a_class):
+    return (issubclass(type(obj), base_class))
