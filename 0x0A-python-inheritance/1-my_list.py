712d74b314da0e8f00efda13840de5c65f6fc589
Ali-TBB <Ali-Tbb@git.com>
2024-02-05 17:52:52 +0100

    task 1

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -8,10 +8,13 @@ Date:
 [2024/02/05]
 """
 
-class MyList:
-        """
-        Prints the list in sorted (ascending) order.
-        """
-        def print_sorted(self):
-            sorted_list = sorted(self)
-            print(sorted_list)
+
+class MyList(list):
+    """a subclass of list"""
+    def __init__(self):
+        """initializes the object"""
+        super().__init__()
+
+    def print_sorted(self):
+        """prints the sorted list"""
+        print(sorted(self))
