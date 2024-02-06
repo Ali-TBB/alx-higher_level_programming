544ad70723dd1a29d002b4b4b6efc5e5af0671ef
Ali-TBB <Ali-Tbb@git.com>
2024-02-06 17:57:41 +0100

    task 7

--- 0x0B-python-input_output/6-load_from_json_file.py
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -22,4 +22,4 @@ def load_from_json_file(filename):
     - object: The Python object represented by the JSON file.
     """
     with open(filename, 'r', encoding='utf-8') as file:
-        return json.load(file)
+        return json.load(file.read())
