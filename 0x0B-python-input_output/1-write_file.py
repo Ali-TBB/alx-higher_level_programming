b6c22db9e1b1590c73f2db52a33d8fea71af1f39
Ali-TBB <Ali-Tbb@git.com>
2024-02-06 10:35:43 +0100

    task 1

---
+++ 0x0B-python-input_output/1-write_file.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""
+append_write function
+function that appends a string at the end of a text file.
+Author:
+[ali debbache]
+Date:
+[2024/02/06]
+"""
+
+
+def append_write(filename="", text=""):
+    """
+    appends a string at the end of a text file.
+
+    Parameters:
+    - filename (str): The path to the file to be read.
+    - text (str): string will be added on the end of text file.
+    Returns:
+    - int: The number of characters added
+    """
+    with open(filename, "a", encoding="utf-8") as file:
+        file.write(text)
+
+        return len(text)
