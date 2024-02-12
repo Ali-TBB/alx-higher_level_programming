a545fec98ac4f4fee20ac38ae161ec5edeaaf266
Ali-TBB <Ali-Tbb@git.com>
2024-02-12 17:32:28 +0100

    0x0C-python-almost_a_circle

--- 0x0C-python-almost_a_circle/main.py
+++ 0x0C-python-almost_a_circle/main.py
@@ -1,15 +1,16 @@
 #!/usr/bin/python3
-""" 16-main """
-from models.rectangle import Rectangle
+""" 10-main """
+from models.square import Square
 
 if __name__ == "__main__":
 
-    list_input = [
-        {'id': 89, 'width': 10, 'height': 4}, 
-        {'id': 7, 'width': 1, 'height': 7}
-    ]
-    json_list_input = Rectangle.to_json_string(list_input)
-    list_output = Rectangle.from_json_string(json_list_input)
-    print("[{}] {}".format(type(list_input), list_input))
-    print("[{}] {}".format(type(json_list_input), json_list_input))
-    print("[{}] {}".format(type(list_output), list_output))
+    s1 = Square(5)
+    print(s1)
+    print(s1.size)
+    s1.size = 10
+    print(s1)
+
+    try:
+        s1.size = "9"
+    except Exception as e:
+        print("[{}] {}".format(e.__class__.__name__, e))
--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -48,7 +48,7 @@ class Square(Rectangle):
     @size.setter
     def size(self, value):
         """Setter for size."""
-        self.__validate_type(value, "size")
+        self._Rectangle__validate_type(value, "size")
         self.height = value
         self.width = value
 
