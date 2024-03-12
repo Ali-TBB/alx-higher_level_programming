2831040e74dfd07b319c2a02f670b122c8c988dd
Ali-TBB <Ali-Tbb@git.com>
2024-03-12 18:16:46 +0100

    0x0D-SQL_introduction

---
+++ 0x0D-SQL_introduction/3-list_tables.sql
@@ -0,0 +1,3 @@
+-- list tables
+USE <database_name>;
+SHOW TABLES;
---
+++ 0x0D-SQL_introduction/4-first_table.sql
@@ -0,0 +1,6 @@
+-- first table
+USE <database_name>;
+CREATE TABLE IF NOT EXISTS first_table (
+    id INT,
+    name VARCHAR(256)
+);
