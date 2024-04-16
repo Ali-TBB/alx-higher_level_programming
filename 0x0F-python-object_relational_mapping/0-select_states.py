abfeb7530846da55121bcb2cac0355549a8f3872
Ali-TBB <Ali-Tbb@git.com>
2024-04-16 10:56:41 +0100

    0x0F-python-object_relational_mapping

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -17,14 +17,14 @@ if __name__ == "__main__":
                          passwd=sys.argv[2],
                          db=sys.argv[3], port=3306)
 
-cursor = db.cursor()
+    cursor = db.cursor()
 
-cursor.execute("SELECT * FROM states ORDER BY id ASC")
+    cursor.execute("SELECT * FROM states ORDER BY id ASC")
 
-states = cursor.fetchall()
+    states = cursor.fetchall()
 
-for state in states:
-    print(state)
+    for state in states:
+        print(state)
 
-cursor.close()
-db.close()
+    cursor.close()
+    db.close()
