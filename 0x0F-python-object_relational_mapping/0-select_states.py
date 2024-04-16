d67086ee725f360c9a648553e58b98c14413077a
Ali-TBB <Ali-Tbb@git.com>
2024-04-16 10:55:41 +0100

    0x0F-python-object_relational_mapping

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+
 """
 Script to list all states from a MySQL database.
 Usage:
@@ -11,12 +12,10 @@ import sys
 
 
 if __name__ == "__main__":
-    username = sys.argv[1]
-    password = sys.argv[2]
-    database = sys.argv[3]
-
-db = MySQLdb.connect(host="localhost", user=username, passwd=password,
-                     db=database, port=3306)
+    db = MySQLdb.connect(host="localhost",
+                         user=sys.argv[1],
+                         passwd=sys.argv[2],
+                         db=sys.argv[3], port=3306)
 
 cursor = db.cursor()
 
