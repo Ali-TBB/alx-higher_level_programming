313840421e03c8a09588c44ec25ef06465cf779b
Ali-TBB <Ali-Tbb@git.com>
2024-04-16 11:16:35 +0100

    0x0F-python-object_relational_mapping

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,1 +1,31 @@
 #!/usr/bin/python3
+
+"""
+Script to list all states from a MySQL database with upper N.
+Usage:
+    python script.py <username> <password> <database>
+"""
+
+
+import MySQLdb
+import sys
+
+
+if __name__ == "__main__":
+    db = MySQLdb.connect(user=sys.argv[1],
+                         passwd=sys.argv[2],
+                         db=sys.argv[3],
+                         host="localhost",
+                         port=3306)
+
+    cursor = db.cursor()
+
+    cursor.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC")
+
+    states = cursor.fetchall()
+
+    for state in states:
+        print(state)
+
+    cursor.close()
+    db.close()
