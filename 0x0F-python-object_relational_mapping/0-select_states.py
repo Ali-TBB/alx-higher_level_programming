914d7ea99c6a0f2f904512e7299215c422430786
Ali-TBB <Ali-Tbb@git.com>
2024-04-16 10:51:31 +0100

    0x0F-python-object_relational_mapping

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -15,23 +15,17 @@ if __name__ == "__main__":
     password = sys.argv[2]
     database = sys.argv[3]
 
-# Connect to MySQL server
-db = MySQLdb.connect(host="localhost",user=username, passwd=password,
+db = MySQLdb.connect(host="localhost", user=username, passwd=password,
                      db=database, port=3306)
 
-# Create a cursor object
 cursor = db.cursor()
 
-# Execute the query to retrieve states
 cursor.execute("SELECT * FROM states ORDER BY id ASC")
 
-# Fetch all the rows
 states = cursor.fetchall()
 
-# Display the results
 for state in states:
     print(state)
 
-# Close cursor and database connection
 cursor.close()
 db.close()
