534e22a898476561174589557abf5fd68ca47865
Ali-TBB <Ali-Tbb@git.com>
2024-04-28 11:31:17 +0100

    0x10-python-network_0

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,10 +1,3 @@
 #!/bin/bash
-# Store the URL provided as an argument
-url=$1
-
-# Send a request to the URL and store the response body in a temporary file
-response=$(curl -sS -s $url)
-size=$(echo -n "$response" | wc -c)
-# Display the size of the response body
-echo "$size"
-
+# Get the byte size of the HTTP response header for a given URL.
+curl -s "$1" | wc -c
