fbdedbbc985dd48b9d4dc753e72d23bca9df8550
Ali-TBB <Ali-Tbb@git.com>
2024-04-30 09:14:36 +0100

    0x11-python-network_1

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -1,1 +1,28 @@
 #!/usr/bin/python3
+"""Sends a POST request to the passed URL with the email as
+a parameter and displays the body of the response."""
+import sys
+import requests
+
+
+def send_email(url, email):
+    """
+    Sends an email via a POST request to the specified URL.
+
+    Args:
+        url (str): The URL to send the email to.
+        email (str): The email content.
+
+    Returns:
+        str: The body of the response.
+    """
+    email_data = {'email': email}
+    response = requests.post(url, data=email_data)
+    print(response.text)
+
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+    email = sys.argv[2]
+
+    send_email(url, email)
