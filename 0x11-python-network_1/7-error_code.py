7b772d47a8ecf9551baa620daa516791f261bb17
Ali-TBB <Ali-Tbb@git.com>
2024-04-30 09:21:53 +0100

    0x11-python-network_1

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -1,1 +1,23 @@
 #!/usr/bin/python3
+"""Fetches a URL and handles HTTP errors."""
+import sys
+import requests
+
+
+def main(url):
+    """
+    Fetches a URL and handles HTTP errors.
+
+    Args:
+        url (str): The URL to fetch.
+    """
+    try:
+        response = requests.get(url)
+        print(requests)
+    except requests.exceptions.HTTPError as e:
+        print("Error code:", e.response.status_code)
+
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+    main(url)
