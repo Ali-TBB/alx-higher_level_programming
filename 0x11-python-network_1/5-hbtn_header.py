8c0ca81e1e51a77284a737d89ba4d301edd39483
Ali-TBB <Ali-Tbb@git.com>
2024-04-30 09:08:54 +0100

    0x11-python-network_1

--- 0x11-python-network_1/5-hbtn_header.py
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -1,1 +1,24 @@
 #!/usr/bin/python3
+""" displays the value of the X-Request-Id
+variable found in the header of the response
+"""
+import sys
+import requests
+
+
+def main(url):
+    """
+    displays the value of the X-Request-Id
+    variable found in the header of the response.
+
+    Args:
+        url (str): The URL to fetch.
+    """
+    response = requests.get(url)
+    x_request_id = response.headers.get('X-Request-Id')
+    print(x_request_id)
+
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+    main(url)
