8b3b0b81cf091ef0ac8dee621b5448c9f8723680
Ali-TBB <Ali-Tbb@git.com>
2024-04-30 08:02:01 +0100

    0x11-python-network_1

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -1,1 +1,14 @@
 #!/usr/bin/python3
+import sys
+import urllib.request
+
+
+def main(url):
+    with urllib.request.urlopen(url) as response:
+        x_request_id = response.getheader('X-Request-Id')
+        print(x_request_id)
+
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+    main(url)
