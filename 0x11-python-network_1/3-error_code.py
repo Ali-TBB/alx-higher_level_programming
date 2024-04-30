1fc270e7e85f3beb3dab02b1378ba7be0d8c9528
Ali-TBB <Ali-Tbb@git.com>
2024-04-30 08:47:59 +0100

    0x11-python-network_1

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -1,1 +1,24 @@
 #!/usr/bin/python3
+"""Fetches a URL and handles HTTP errors."""
+import sys
+import urllib.request
+import urllib.error
+
+
+def main(url):
+    """
+    Fetches a URL and handles HTTP errors.
+
+    Args:
+        url (str): The URL to fetch.
+    """
+    try:
+        with urllib.request.urlopen(url) as response:
+            print(response.read().decode("ascii"))
+    except urllib.error.HTTPError as e:
+        print("Error code: {}".format(e.code))
+
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+    main(url)
